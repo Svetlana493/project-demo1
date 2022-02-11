ad0ee692dcceb9a23a4ea1c05c61752179f095c7
Svetlana493 <93873804+Svetlana493@users.noreply.github.com>
2022-02-11 16:14:50 +0000

    Deploying to gh-pages from @ Svetlana493/project-demo1@24911039b85db35e593e00a331989c0d149f1267 🚀

--- src.68b61447.js
+++ src.68b61447.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-demo1/src.8600c03b.js.map
+//# sourceMappingURL=/project-demo1/src.68b61447.js.map
